94e3073f2eb2d9571e97ca63c768714a14f0c5b6
Katherine Lou <katherinehmlou@gmail.com>
2019-11-21 12:49:50 -0500

    add people

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,19 @@ var express = require('express');
 var router = express.Router();
 var Sentencer = require('sentencer');
 
+var people = ["Lara", "Kat", "Sophie", "Chloe", "Sarah", "Nancy", "Rob"];
+
+Sentencer.configure({
+  actions: {
+    person: function() {
+      return people[Math.floor(Math.random() * 7)];
+    }
+  }
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var lg = Sentencer.make("This sentence has {{ a_noun }} and {{ an_adjective }} {{ noun }} in it.");
+  var lg = Sentencer.make("{{ person }} has {{ a_noun }} and {{ an_adjective }} {{ noun }}.");
   res.render('index', { out: lg });
 });
 
