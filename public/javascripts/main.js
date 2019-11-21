3c49c29e470819c90f1cee687250791cac60b415
Katherine Lou <katherinehmlou@gmail.com>
2019-11-21 14:45:05 -0500

    forgot to commit mainjs

---
+++ public/javascripts/main.js
@@ -0,0 +1,38 @@
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var c = getCookie("count");
+  var c_int = parseInt(c, 10);
+  if (c == "") {
+   setCookie("count", 0, 365);
+   c_int = 0;
+ }
+ document.getElementById("counter").innerHTML = "You've generated " + c_int + " lovely generalizations.";
+}
+
+function lovelyGen() {
+  var c_int = parseInt(getCookie("count"), 10) + 1;
+  setCookie("count", c_int, 365);
+  window.location.reload();
+}
