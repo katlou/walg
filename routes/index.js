1cded92e15dd9e0e14cdf0f6880df4b92b0e92e4
Katherine Lou <katherinehmlou@gmail.com>
2019-11-20 19:14:50 -0500

    proof of concept

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var Sentencer = require('sentencer');
 
-var lg = Sentencer.make("This sentence has {{ a_noun }} and {{ an_adjective }} {{ noun }} in it.");
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var lg = Sentencer.make("This sentence has {{ a_noun }} and {{ an_adjective }} {{ noun }} in it.");
   res.render('index', { out: lg });
 });
 
